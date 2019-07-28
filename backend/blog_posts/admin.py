998d4627db7465e0d0453b476aa1115e5923289c
Vlad Vitan <vladvitan@gmail.com>
2019-07-29 01:02:35 +0300

    adding basic functionality for managing parteners

--- backend/blog_posts/admin.py
+++ backend/blog_posts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import BlogPost
+from .models import Partener
 
 admin.site.register(BlogPost)
+admin.site.register(Partener)
--- backend/blog_posts/models.py
+++ backend/blog_posts/models.py
@@ -12,3 +12,12 @@ class BlogPost(models.Model):
 
     def __unicode__(self):
         return self.title
+
+
+class Partener(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    logo = models.ImageField()
+    group = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
