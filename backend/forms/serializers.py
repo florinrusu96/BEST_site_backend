5f83b08f35225de21b579f2900f2cbc5ba4ce0d3
Rusu Florin Andrei <rusuflorinandrei96@gmail.com>
2019-09-10 19:32:01 +0300

    Added form models and send email functionality, no reason to save contact mails to database, STILL NEED TO ADD REAL EMAIL INFO (#2)

---
+++ backend/forms/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from .models import Contact
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = (
+            "name",
+            "email",
+            "subject",
+            "content",
+        )
---
+++ backend/forms/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from . import views
+
+urlpatterns = [
+    path("forms/", views.ContactView.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
