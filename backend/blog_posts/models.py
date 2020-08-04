aec237e8877becf4869c22fbd5e5a3e6f5600383
florinrusu96 <rusuflorinandrei96@gmail.com>
2020-08-04 10:04:03 -0700

    RIch text editor to blog posts admin panel

--- backend/blog_posts/models.py
+++ backend/blog_posts/models.py
@@ -1,14 +1,13 @@
-from django.contrib.auth.models import User
 from django.db import models
-
+from tinymce.models import HTMLField
 
 class BlogPost(models.Model):
     author = models.ForeignKey(
         "auth.User", related_name="blog_posts", on_delete=models.CASCADE
     )
     title = models.CharField(max_length=64, unique=True)
-    content = models.TextField(default=None)
-    english_content = models.TextField(default=None)
+    content = HTMLField()
+    english_content = HTMLField()
     updated_on = models.DateField(auto_now=True)
     created_on = models.DateField(auto_now_add=True)
 
--- backend/blog_posts/urls.py
+++ backend/blog_posts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path("blog-posts/<int:pk>/", views.BlogPostDetail.as_view()),
     path("users/", views.UserList.as_view()),
     path("users/<int:pk>/", views.UserDetail.as_view()),
+    path('tinymce/', include('tinymce.urls')),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
